1a7589cb067fcf2a87f8004c3fa53dfc688c0f80
mollychin <mollychin36@qq.com>
2016-11-22 16:20:00 +0800

    add Test.java

---
+++ server/4FunServer/src/com/mollychin/spider/Test.java
@@ -0,0 +1,12 @@
+package com.mollychin.spider;
+
+public class Test {
+	public static void main(String[] args) {
+		ArDailySpider.main(args);
+		ArOneSpider.main(args);
+		MovieParser.main(args);
+		MusicParser.main(args);
+		PicOneSpider.main(args);
+		PoemSpider.main(args);
+	}
+}
