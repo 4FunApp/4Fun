babbddb0cdc4093ba8b94e0f9fbfaa6dd5ed99a0
jokermonn <429094465@qq.com>
2016-11-29 12:25:01 +0800

    net data RUP

---
+++ client/FourFun/app/src/main/java/com/joker/fourfun/net/HttpResultFun.java
@@ -0,0 +1,18 @@
+package com.joker.fourfun.net;
+
+import io.reactivex.functions.Function;
+
+/**
+ * Created by joker on 2016/11/28.
+ */
+
+public class HttpResultFun<T> implements Function<HttpResult<T>, T> {
+    @Override
+    public T apply(HttpResult<T> tHttpResult) throws Exception {
+        if (tHttpResult.isError()) {
+            throw new ApiException(ApiException.MESSAGE_NOT_FOUND);
+        }
+
+        return tHttpResult.getResult();
+    }
+}
