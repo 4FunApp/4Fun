5b866fecfddb6149ab71813a1be91f799da63cb5
jokermonn <429094465@qq.com>
2016-11-29 12:29:11 +0800

    Dagger2 custom annotation for fragment

---
+++ client/FourFun/app/src/main/java/com/joker/fourfun/di/PerFragment.java
@@ -0,0 +1,14 @@
+package com.joker.fourfun.di;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+/**
+ * Created by joker on 2016/11/28.
+ */
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PerFragment {
+}
