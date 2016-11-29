9fc3f8ea725756b2939e65b829340c38d026b9ad
jokermonn <429094465@qq.com>
2016-11-29 12:28:07 +0800

    System api encapsulation

---
+++ client/FourFun/app/src/main/java/com/joker/fourfun/utils/SystemUtil.java
@@ -0,0 +1,39 @@
+package com.joker.fourfun.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.widget.Toast;
+
+import com.joker.fourfun.FourFun;
+
+/**
+ * Created by joker on 2016/11/27.
+ */
+public class SystemUtil {
+    private static Toast mToast;
+
+    /**
+     * 检查是否有可用网络
+     */
+    public static boolean isNetworkConnected() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) FourFun.getInstance()
+                .getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        return connectivityManager.getActiveNetworkInfo() != null;
+    }
+
+    public static void showToast(Context context, String content) {
+        if (mToast == null) {
+            mToast = Toast.makeText(context, content, Toast.LENGTH_SHORT);
+        } else {
+            mToast.setText(content);
+        }
+
+        mToast.show();
+    }
+
+    public static void cancelToast() {
+        if (mToast != null) {
+            mToast.cancel();
+        }
+    }
+}
