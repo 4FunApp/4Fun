503c42818655d8a5338e1bb31602e7e511a482fc
jokermonn <429094465@qq.com>
2016-12-03 20:33:26 +0800

    add FragmentComponent

---
+++ client/FourFun/app/src/main/java/com/joker/fourfun/di/component/FragmentComponent.java
@@ -0,0 +1,19 @@
+package com.joker.fourfun.di.component;
+
+import com.joker.fourfun.di.PerFragment;
+import com.joker.fourfun.di.module.FragmentModule;
+import com.joker.fourfun.ui.fragment.PictureFragment;
+
+import dagger.Component;
+import me.yokeyword.fragmentation.SupportFragment;
+
+/**
+ * Created by joker on 2016/12/1.
+ */
+@PerFragment
+@Component(dependencies = AppComponent.class, modules = FragmentModule.class)
+public interface FragmentComponent {
+    SupportFragment fragment();
+
+    void inject(PictureFragment fragment);
+}
