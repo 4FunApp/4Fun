4fde3237c5ba218d5cb7ca57b20aa51299fc3784
jokermonn <429094465@qq.com>
2016-12-03 20:33:56 +0800

    add FragmentModule

---
+++ client/FourFun/app/src/main/java/com/joker/fourfun/di/module/FragmentModule.java
@@ -0,0 +1,25 @@
+package com.joker.fourfun.di.module;
+
+import com.joker.fourfun.di.PerFragment;
+
+import dagger.Module;
+import dagger.Provides;
+import me.yokeyword.fragmentation.SupportFragment;
+
+/**
+ * Created by joker on 2016/12/1.
+ */
+@Module
+public class FragmentModule {
+    private SupportFragment mFragment;
+
+    public FragmentModule(SupportFragment fragment) {
+        mFragment = fragment;
+    }
+
+    @Provides
+    @PerFragment
+    SupportFragment provideFragment() {
+        return mFragment;
+    }
+}
