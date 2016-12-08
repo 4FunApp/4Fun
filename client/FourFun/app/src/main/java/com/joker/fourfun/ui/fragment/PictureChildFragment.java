91e7c65339dfbff4449e845eafb91077b2c5b62b
jokermonn <429094465@qq.com>
2016-12-08 14:41:42 +0800

    add field

--- client/FourFun/app/src/main/java/com/joker/fourfun/ui/fragment/PictureChildFragment.java
+++ client/FourFun/app/src/main/java/com/joker/fourfun/ui/fragment/PictureChildFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.joker.fourfun.R;
 import com.joker.fourfun.base.BaseMvpFragment;
@@ -22,7 +23,16 @@ public class PictureChildFragment extends BaseMvpFragment<PictureChildContract.V
         PictureChildPresenter> implements PictureChildContract.View {
     @BindView(R.id.iv_content)
     ImageView mIvContent;
-    private int mPosition = 0;
+    @BindView(R.id.tv_VOL)
+    TextView mTvVOL;
+    @BindView(R.id.tv_author)
+    TextView mTvAuthor;
+    @BindView(R.id.tv_des)
+    TextView mTvDes;
+    @BindView(R.id.tv_date)
+    TextView mTvDate;
+    // 网页更新慢一天，所以默认值为 1
+    private int mDay = 1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,16 +47,21 @@ public class PictureChildFragment extends BaseMvpFragment<PictureChildContract.V
     public void onLazyInitView(@Nullable Bundle savedInstanceState) {
         super.onLazyInitView(savedInstanceState);
         Logger.e("onLazyInitView");
-        mPresenter.getContent(mPosition * -1);
+        mPresenter.getContent(mDay * -1);
     }
 
     public void setFragmentPosition(int position) {
-        mPosition = position;
+        // 网页更新慢一天 故要 +1
+        mDay = position + 1;
     }
 
     @Override
     public void showContent(PicOne picOne) {
         GlideUtil.setImage(mActivity, picOne.getPicUrl(), mIvContent);
+        mTvAuthor.setText(picOne.getAuthorWork());
+        mTvVOL.setText(picOne.getVOL());
+        mTvDes.setText(picOne.getPicDescription());
+        mTvDate.setText(picOne.getPicDate());
     }
 
     @Override
