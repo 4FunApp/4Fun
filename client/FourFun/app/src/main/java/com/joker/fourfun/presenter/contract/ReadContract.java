7c143842218e3724281142005784a1a2b84b86a9
jokermonn <429094465@qq.com>
2016-12-18 18:56:23 +0800

    add interface

--- client/FourFun/app/src/main/java/com/joker/fourfun/presenter/contract/ReadContract.java
+++ client/FourFun/app/src/main/java/com/joker/fourfun/presenter/contract/ReadContract.java
@@ -2,18 +2,18 @@ package com.joker.fourfun.presenter.contract;
 
 import com.joker.fourfun.base.BasePresenter;
 import com.joker.fourfun.base.BaseView;
-import com.joker.fourfun.model.Picture;
+import com.joker.fourfun.model.ArticleOne;
 
 /**
- * Created by joker on 2016/12/1.
+ * Created by joker on 2016/12/8.
  */
 
-public interface PictureChildContract {
+public interface ReadContract {
     interface View extends BaseView {
-        void showContent(Picture picture);
+        void showArticle(ArticleOne article);
     }
 
     interface Presenter extends BasePresenter<View> {
-        void getContent(int before);
+        void getArticle(int num);
     }
 }
