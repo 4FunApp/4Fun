97158fc196a8272c2beebe45e5071834e925e450
jokermonn <429094465@qq.com>
2016-12-18 16:51:49 +0800

    add typeface

--- client/FourFun/app/src/main/java/com/joker/fourfun/utils/SystemUtil.java
+++ client/FourFun/app/src/main/java/com/joker/fourfun/utils/SystemUtil.java
@@ -1,6 +1,7 @@
 package com.joker.fourfun.utils;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.net.ConnectivityManager;
 import android.widget.Toast;
 
@@ -15,10 +16,15 @@ import java.util.Locale;
  * Created by joker on 2016/11/27.
  */
 public class SystemUtil {
+    public static final String RUNNING_FONT = "running_font.ttf";
     private static Toast mToast;
+
+    private SystemUtil() {
+    }
 
     /**
      * 网络是否连接
+     *
      * @return
      */
     public static boolean isNetworkConnected() {
@@ -29,6 +35,7 @@ public class SystemUtil {
 
     /**
      * toast 优化显示
+     *
      * @param context
      * @param content
      */
@@ -53,6 +60,7 @@ public class SystemUtil {
 
     /**
      * 前几天的日期
+     *
      * @param before 和今天相差的天数
      * @return
      */
@@ -68,5 +76,54 @@ public class SystemUtil {
         }
 
         return sdf.format(now);
+    }
+
+    /**
+     * 像素转 sp
+     *
+     * @param context
+     * @param textSizePixel
+     * @return
+     */
+    public static int px2sp(Context context, float textSizePixel) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (textSizePixel / fontScale + 0.5f);
+    }
+
+    /**
+     * sp 转像素
+     * @param context
+     * @param textSizeSp
+     * @return
+     */
+    public static float sp2px(Context context, int textSizeSp) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (textSizeSp * fontScale + 0.5f);
+    }
+
+    /**
+     * dip 转像素
+     * @param context
+     * @param dpValue
+     * @return
+     */
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * 像素转 dip
+     * @param context
+     * @param pxValue
+     * @return
+     */
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    public static Typeface getTypeface(Context context) {
+        return Typeface.createFromAsset(context.getAssets(), "fonts/" + RUNNING_FONT);
     }
 }
