fc9e888f8d4ff36982b1399f10a693f8efb03707
jokermonn <429094465@qq.com>
2016-12-20 17:50:15 +0800

    add circle imageview operation

--- client/FourFun/app/src/main/java/com/joker/fourfun/utils/GlideUtil.java
+++ client/FourFun/app/src/main/java/com/joker/fourfun/utils/GlideUtil.java
@@ -5,18 +5,30 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.joker.fourfun.view.GlideCircleTransform;
 
 /**
  * Created by joker on 2016/12/6.
  */
 
 public class GlideUtil {
+    // 磁盘缓存图片所有大小，centerCrop 大小
     public static void setImage(Context context, String imageUrl, ImageView targetView) {
         Glide.with(context)
                 .load(imageUrl)
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
 //                .thumbnail(0.5f)
                 .centerCrop()
+                .into(targetView);
+    }
+
+    //圆形 ImageView
+    public static void setCirCleImage(Context context, String imageUrl, ImageView targetView) {
+        Glide.with(context)
+                .load(imageUrl)
+                .transform(new GlideCircleTransform(context))
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .crossFade()
                 .into(targetView);
     }
 }
