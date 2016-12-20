b0b0a5143210d9f949cdc7e5c66d1efd37afcecf
jokermonn <429094465@qq.com>
2016-12-20 17:54:20 +0800

    global constants

---
+++ client/FourFun/app/src/main/java/com/joker/fourfun/Constants.java
@@ -0,0 +1,11 @@
+package com.joker.fourfun;
+
+/**
+ * Created by joker on 2016/12/20.
+ */
+
+public class Constants {
+    public static final String MEDIA_BUNDLE = "media_bundle";
+    public static final String ZHIHU_IMG = "zhihu_img";
+    public static final String PICTURE_BUNDLE = "picture_bundle";
+}
