248c5d2178ed0a33f6a83b4232b8718124ae06fb
jokermonn <429094465@qq.com>
2016-12-30 19:14:24 +0800

    delete odd code

--- client/FourFun/app/src/main/java/com/joker/fourfun/ui/fragment/PictureFragment.java
+++ client/FourFun/app/src/main/java/com/joker/fourfun/ui/fragment/PictureFragment.java
@@ -6,14 +6,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.joker.fourfun.Constants;
 import com.joker.fourfun.R;
-import com.joker.fourfun.adapter.PicOneAdapter;
+import com.joker.fourfun.adapter.PictureOneAdapter;
 import com.joker.fourfun.base.BaseMvpFragment;
 import com.joker.fourfun.presenter.PicturePresenter;
 import com.joker.fourfun.presenter.contract.PictureContract;
 import com.joker.fourfun.utils.SystemUtil;
 import com.joker.fourfun.widget.ZoomOutPageTransformer;
-import com.orhanobut.logger.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,31 +25,24 @@ public class PictureFragment extends BaseMvpFragment<PictureContract.View, Pictu
         PictureContract.View {
     @BindView(R.id.vp_content)
     ViewPager mVpContent;
-    private PicOneAdapter mAdapter;
+    private Bundle mPictureBundle;
     private List<PictureChildFragment> mFragments;
+
+    public static PictureFragment newInstance(Bundle bundle) {
+        PictureFragment pictureFragment = new PictureFragment();
+        pictureFragment.setArguments(bundle);
+
+        return pictureFragment;
+    }
 
     @Override
     protected View createView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        analyseArguments();
         View view = inflater.inflate(R.layout.fragment_picture, container, false);
         ButterKnife.bind(this, view);
         mVpContent.setOffscreenPageLimit(1);
-        mVpContent.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-//                mPresenter.getContent((position + 1) * -1);
-                Logger.i(position + "selected", position + "selected");
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-            }
-        });
         mFragments = new ArrayList<PictureChildFragment>();
-        mFragments.add(new PictureChildFragment());
+        mFragments.add(PictureChildFragment.newInstance(mPictureBundle));
         mFragments.add(new PictureChildFragment());
         mFragments.add(new PictureChildFragment());
         mFragments.add(new PictureChildFragment());
@@ -58,26 +51,15 @@ public class PictureFragment extends BaseMvpFragment<PictureContract.View, Pictu
         mFragments.add(new PictureChildFragment());
         mFragments.add(new PictureChildFragment());
         mVpContent.setPageTransformer(true, new ZoomOutPageTransformer());
-        mVpContent.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-//                Logger.e("the " + position + " page is selected", "");
-                mFragments.get(position).setFragmentPosition(position);
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-            }
-        });
-        mAdapter = new PicOneAdapter(getChildFragmentManager(), mFragments);
-        mVpContent.setOffscreenPageLimit(1);
+        PictureOneAdapter mAdapter = new PictureOneAdapter(getChildFragmentManager(), mFragments);
         mVpContent.setAdapter(mAdapter);
 
         return view;
+    }
+
+    private void analyseArguments() {
+        Bundle bundle = getArguments();
+        mPictureBundle = bundle.getBundle(Constants.PICTURE_BUNDLE);
     }
 
     @Override
@@ -88,9 +70,5 @@ public class PictureFragment extends BaseMvpFragment<PictureContract.View, Pictu
     @Override
     public void showError(String message) {
         SystemUtil.showToast(mActivity, message);
-    }
-
-    public static PictureFragment newInstance() {
-        return null;
     }
 }
