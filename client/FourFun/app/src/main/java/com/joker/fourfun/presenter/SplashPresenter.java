0f6008d272efd266344f3d2cd612696c0931cb9e
jokermonn <429094465@qq.com>
2016-12-30 19:05:50 +0800

    refactor code

--- client/FourFun/app/src/main/java/com/joker/fourfun/presenter/SplashPresenter.java
+++ client/FourFun/app/src/main/java/com/joker/fourfun/presenter/SplashPresenter.java
@@ -1,12 +1,14 @@
 package com.joker.fourfun.presenter;
 
 import com.joker.fourfun.base.BaseMvpPresenter;
+import com.joker.fourfun.model.Picture;
 import com.joker.fourfun.model.Zhihu;
 import com.joker.fourfun.net.HttpResultFun;
 import com.joker.fourfun.net.UserService;
 import com.joker.fourfun.presenter.contract.SplashContract;
 import com.joker.fourfun.utils.RetrofitUtil;
 import com.joker.fourfun.utils.RxUtil;
+import com.joker.fourfun.utils.SystemUtil;
 import com.orhanobut.logger.Logger;
 
 import org.reactivestreams.Subscriber;
@@ -16,6 +18,8 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import io.reactivex.functions.Consumer;
+
 
 /**
  * Created by joker on 2016/11/27.
@@ -23,20 +27,20 @@ import javax.inject.Inject;
 
 public class SplashPresenter extends BaseMvpPresenter<SplashContract.View> implements SplashContract
         .Presenter {
-    private RetrofitUtil mRetrofit;
+    private UserService mService;
 
     @Inject
     SplashPresenter(RetrofitUtil retrofitUtil) {
-        mRetrofit = retrofitUtil;
+        mService = retrofitUtil.create(UserService.class);
     }
 
     @Override
     public void getZhihuPic() {
-        UserService userService = mRetrofit.create(UserService.class);
-        userService
+        mService
                 .zhihuPic()
                 .map(new HttpResultFun<List<Zhihu>>())
                 .compose(RxUtil.<List<Zhihu>>rxSchedulerTransformer())
+                .compose(RxUtil.<List<Zhihu>>rxTimeoutTransformer())
                 .subscribe(new Subscriber<List<Zhihu>>() {
                     @Override
                     public void onSubscribe(Subscription s) {
@@ -46,6 +50,7 @@ public class SplashPresenter extends BaseMvpPresenter<SplashContract.View> imple
                     @Override
                     public void onNext(List<Zhihu> zhihu) {
                         mView.showZhihuPic(zhihu.get(0).getImg());
+                        mView.setMediaBackground(zhihu.get(0).getImg());
                     }
 
                     @Override
@@ -56,6 +61,26 @@ public class SplashPresenter extends BaseMvpPresenter<SplashContract.View> imple
 
                     @Override
                     public void onComplete() {
+                    }
+                });
+    }
+
+    @Override
+    public void getPictureOne(int before) {
+        String date = SystemUtil.beforeToday(before);
+        mService.picOne(date)
+                .map(new HttpResultFun<List<Picture>>())
+                .compose(RxUtil.<List<Picture>>rxSchedulerTransformer())
+                .subscribe(new Consumer<List<Picture>>() {
+                    @Override
+                    public void accept(List<Picture> list) throws Exception {
+                        mView.setPictureOne(list.get(0));
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        Logger.e(throwable, "ERROR");
+                        mView.showError("似乎有点问题哦");
                     }
                 });
     }
--- client/FourFun/app/src/main/java/com/joker/fourfun/presenter/contract/SplashContract.java
+++ client/FourFun/app/src/main/java/com/joker/fourfun/presenter/contract/SplashContract.java
@@ -2,6 +2,7 @@ package com.joker.fourfun.presenter.contract;
 
 import com.joker.fourfun.base.BasePresenter;
 import com.joker.fourfun.base.BaseView;
+import com.joker.fourfun.model.Picture;
 
 /**
  * Created by joker on 2016/11/27.
@@ -10,9 +11,15 @@ import com.joker.fourfun.base.BaseView;
 public interface SplashContract {
     interface View extends BaseView {
         void showZhihuPic(String url);
+
+        void setMediaBackground(String url);
+
+        void setPictureOne(Picture picture);
     }
 
     interface Presenter extends BasePresenter<View> {
         void getZhihuPic();
+
+        void getPictureOne(int before);
     }
 }
