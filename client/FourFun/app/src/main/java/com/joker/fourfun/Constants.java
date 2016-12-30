54af281d313b3feb0f39091fb568cdcef5a65499
jokermonn <429094465@qq.com>
2016-12-30 19:19:52 +0800

    add some constants

--- client/FourFun/app/src/main/java/com/joker/fourfun/Constants.java
+++ client/FourFun/app/src/main/java/com/joker/fourfun/Constants.java
@@ -5,7 +5,13 @@ package com.joker.fourfun;
  */
 
 public class Constants {
+    public static final String MAIN_ACTIVITY_BUNDLE = "main_bundle";
     public static final String MEDIA_BUNDLE = "media_bundle";
     public static final String ZHIHU_IMG = "zhihu_img";
     public static final String PICTURE_BUNDLE = "picture_bundle";
+    public static final String PICTURE_ONE_IMG = "picture_one";
+    public static final String PICTURE_DETAILS_BUNDLE = "picture_details_bundle";
+    public static final String PICTURE_DETAILS_ONE_POSITION = "picture_one_details";
+    public static final String PICTURE_DETAILS_IMG = "picture_details_one";
+    public static final String TRANSIT_PIC = "transit_pic";
 }
