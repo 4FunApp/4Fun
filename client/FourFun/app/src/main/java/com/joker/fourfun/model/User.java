bc89c46a7e54e7ebc3b5b9dbd5c4d1806f1e9e32
jokermonn <429094465@qq.com>
2016-12-30 18:58:12 +0800

    add login and register function

---
+++ client/FourFun/app/src/main/java/com/joker/fourfun/model/User.java
@@ -0,0 +1,53 @@
+package com.joker.fourfun.model;
+
+/**
+ * Created by joker on 2016/12/30.
+ */
+
+public class User {
+    private int userid;
+    private String username;
+    private String password;
+    private int sex;
+    private String email;
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
