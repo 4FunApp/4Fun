cea771588f2f8991fb700cb36d520971da977f2e
jokermonn <429094465@qq.com>
2016-12-30 18:58:35 +0800

    data from server

---
+++ client/FourFun/app/src/main/java/com/joker/fourfun/model/LoginInfo.java
@@ -0,0 +1,25 @@
+package com.joker.fourfun.model;
+
+/**
+ * Created by joker on 2016/12/30.
+ */
+
+public class LoginInfo {
+    /**
+     * error : false
+     * info : [{"msg":"用户名已经注册"}]
+     */
+    /**
+     * msg : 用户名已经注册
+     */
+
+    private String msg;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
