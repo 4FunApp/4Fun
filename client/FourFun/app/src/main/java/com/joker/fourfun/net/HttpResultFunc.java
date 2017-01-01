22c5431cefad0253b40d4f658011abb58d91d890
jokermonn <429094465@qq.com>
2017-01-01 10:05:25 +0800

    rename

---
+++ client/FourFun/app/src/main/java/com/joker/fourfun/net/HttpResultFunc.java
@@ -0,0 +1,21 @@
+package com.joker.fourfun.net;
+
+import io.reactivex.functions.Function;
+
+/**
+ * Created by joker on 2016/11/28.
+ */
+
+public class HttpResultFunc<T> implements Function<HttpResult<T>, T> {
+    @Override
+    public T apply(HttpResult<T> tHttpResult) throws Exception {
+        if (tHttpResult == null) {
+            throw new ConnectErrorException(ConnectErrorException.TIME_OUT_ERROR);
+        }
+        if (tHttpResult.isError()) {
+            throw new ApiException(ApiException.MESSAGE_NOT_FOUND);
+        }
+
+        return tHttpResult.getResult();
+    }
+}
