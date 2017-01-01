d17aef02069e3f09f9cb21d06a127b9a606229f7
jokermonn <429094465@qq.com>
2017-01-01 10:03:19 +0800

    add error state code

--- client/FourFun/app/src/main/java/com/joker/fourfun/Constants.java
+++ client/FourFun/app/src/main/java/com/joker/fourfun/Constants.java
@@ -14,4 +14,16 @@ public class Constants {
     public static final String PICTURE_DETAILS_ONE_POSITION = "picture_one_details";
     public static final String PICTURE_DETAILS_IMG = "picture_details_one";
     public static final String TRANSIT_PIC = "transit_pic";
+
+    // 注册登录状态码
+    public static final int REGISTER_USER_REPEAT_ERROR_CODE = 1027;
+
+    public static final int REGISTER_SUCCESS_CODE = 1050;
+    public static final int LOGIN_SUCCESS_CODE = 1051;
+
+    // 注册登录状态信息
+    public static final String REGISTER_USER_REPEAT_ORROR_MESSAGE = "用户已注册";
+
+    public static final String REGISTER_SUCCESS_MESSAGE = "注册成功";
+    public static final String LOGIN_SUCCESS_MESSAGE = "注册成功";
 }
