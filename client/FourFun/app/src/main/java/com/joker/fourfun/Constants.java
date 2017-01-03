583c0af5213f9373f3790b6faf185ccb2f494892
jokermonn <429094465@qq.com>
2017-01-03 19:50:03 +0800

    add contants

--- client/FourFun/app/src/main/java/com/joker/fourfun/Constants.java
+++ client/FourFun/app/src/main/java/com/joker/fourfun/Constants.java
@@ -7,23 +7,28 @@ package com.joker.fourfun;
 public class Constants {
     public static final String MAIN_ACTIVITY_BUNDLE = "main_bundle";
     public static final String MEDIA_BUNDLE = "media_bundle";
-    public static final String ZHIHU_IMG = "zhihu_img";
+    public static final String MOVIE_BUNDLE = "movie_bundle";
     public static final String PICTURE_BUNDLE = "picture_bundle";
-    public static final String PICTURE_ONE_IMG = "picture_one";
     public static final String PICTURE_DETAILS_BUNDLE = "picture_details_bundle";
-    public static final String PICTURE_DETAILS_ONE_POSITION = "picture_one_details";
+    public static final String ZHIHU_IMG = "zhihu_img";
+    public static final String PICTURE_ONE_IMG = "picture_one";
     public static final String PICTURE_DETAILS_IMG = "picture_details_one";
+    public static final String MOVIE_DETAILS_BEAN = "movie_detail_bean";
+    public static final String PICTURE_DETAILS_ONE_POSITION = "picture_one_details";
     public static final String TRANSIT_PIC = "transit_pic";
 
     // 注册登录状态码
-    public static final int REGISTER_USER_REPEAT_ERROR_CODE = 1027;
-
     public static final int REGISTER_SUCCESS_CODE = 1050;
     public static final int LOGIN_SUCCESS_CODE = 1051;
 
     // 注册登录状态信息
-    public static final String REGISTER_USER_REPEAT_ORROR_MESSAGE = "用户已注册";
-
     public static final String REGISTER_SUCCESS_MESSAGE = "注册成功";
-    public static final String LOGIN_SUCCESS_MESSAGE = "注册成功";
+    public static final String LOGIN_SUCCESS_MESSAGE = "登录成功";
+
+    // 正则
+    // 非空，非空格
+    public static final String REGEXP_EMPTY = "[^\\s]{1,}";
+    // 邮箱
+    public static final String REGEXP_EMAIL = "\\w[-\\w.+]*@([A-Za-z0-9][-A-Za-z0-9]+\\.)+[A-Za-z]{2,14}";
+
 }
