4172e6c5894cf5d93676d89ea8721609517289a5
jokermonn <429094465@qq.com>
2017-01-03 19:54:43 +0800

    add an error code

--- client/FourFun/app/src/main/java/com/joker/fourfun/net/LoginException.java
+++ client/FourFun/app/src/main/java/com/joker/fourfun/net/LoginException.java
@@ -7,8 +7,11 @@ package com.joker.fourfun.net;
 public class LoginException extends RuntimeException {
     public static final int LOGIN_PASSWORD_ERROR_CODE = 1025;
     public static final int LOGIN_USER_ERROR_CODE = 1026;
+    public static final int REGISTER_USER_REPEAT_ERROR_CODE = 1027;
     public static final String LOGIN_PASSWORD_ERROR_MESSAGE = "密码错误，请重试";
     public static final String LOGIN_USER_ERROR_MESSAGE = "用户名不存在";
+    public static final String REGISTER_USER_REPEAT_ERROR_MESSAGE = "用户已注册";
+
 
     public LoginException(int code) {
         this(getLoginExceptionMessage(code));
@@ -26,6 +29,9 @@ public class LoginException extends RuntimeException {
                 break;
             case LOGIN_USER_ERROR_CODE:
                 message = LOGIN_USER_ERROR_MESSAGE;
+                break;
+            case REGISTER_USER_REPEAT_ERROR_CODE:
+                message = REGISTER_USER_REPEAT_ERROR_MESSAGE;
                 break;
             default:
                 message = "未知错误";
