5849da9f68c1a7036337931c07830785d5adc1e7
jokermonn <429094465@qq.com>
2017-01-03 19:56:57 +0800

    refactor code

--- client/FourFun/app/src/main/java/com/joker/fourfun/ui/MainActivity.java
+++ client/FourFun/app/src/main/java/com/joker/fourfun/ui/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.transition.Explode;
 import android.view.Gravity;
 import android.view.View;
@@ -15,10 +16,12 @@ import android.widget.RadioButton;
 import com.joker.fourfun.Constants;
 import com.joker.fourfun.R;
 import com.joker.fourfun.base.BaseMvpFragment;
+import com.joker.fourfun.login.LoginContext;
 import com.joker.fourfun.ui.fragment.MediaFragment;
 import com.joker.fourfun.ui.fragment.MineFragment;
 import com.joker.fourfun.ui.fragment.PictureFragment;
 import com.joker.fourfun.ui.fragment.ReadFragment;
+import com.joker.fourfun.ui.mode.ThemeModeContext;
 import com.nightonke.boommenu.BoomButtons.OnBMClickListener;
 import com.nightonke.boommenu.BoomButtons.TextOutsideCircleButton;
 import com.nightonke.boommenu.BoomMenuButton;
@@ -48,6 +51,8 @@ public class MainActivity extends SupportActivity {
     BoomMenuButton mBmbContent;
     private Bundle mMediaBundle;
     private int showingPosition = FIRST;
+    private int mDefaultUIFlag;
+    private View mDecorView;
     private BaseMvpFragment[] mFragments;
 
     @Override
@@ -57,6 +62,10 @@ public class MainActivity extends SupportActivity {
             getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
             getWindow().setExitTransition(new Explode());
         }
+        mDecorView = getWindow().getDecorView();
+        mDefaultUIFlag = mDecorView.getSystemUiVisibility() | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.main_background));
+//        setAppTheme();
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         ButterKnife.bind(this);
@@ -64,42 +73,18 @@ public class MainActivity extends SupportActivity {
         initChildViews(savedInstanceState);
     }
 
+    private void setAppTheme() {
+        ThemeModeContext.getInstance().setAppTheme(this);
+    }
+
     private void initChildViews(Bundle savedInstanceState) {
-        String[] texts = {getString(R.string.login_register), getString(R.string.setting), getString(R
+        String[] texts = {getString(R.string.personal_center), getString(R.string.setting), getString(R
                 .string
                 .collection), getString(R.string.submit)};
         // 蓝，红，绿，黄
-        int[] colors = {Color.rgb(0, 188, 212), Color.rgb(255, 205, 210), Color.rgb(144, 238, 144), Color
-                .rgb(255, 249, 196)};
-        for (int i = 0; i < mBmbContent.getButtonPlaceEnum().buttonNumber(); i++) {
-            TextOutsideCircleButton.Builder builder = new TextOutsideCircleButton.Builder()
-                    .normalImageRes(R.drawable.jelly_fish)
-                    .normalText(texts[i])
-                    .normalColor(colors[i])
-                    .textGravity(Gravity.CENTER)
-                    .rotateImage(true)
-                    .shadowEffect(true)
-                    .listener(new OnBMClickListener() {
-                        @Override
-                        public void onBoomButtonClick(int index) {
-                            switch (index) {
-                                case 0:
-                                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
-                                    break;
-                                case 1:
-                                    break;
-                                case 2:
-                                    break;
-                                case 3:
-                                    break;
-                                default:
-                                    break;
-                            }
-                        }
-                    });
-
-            mBmbContent.addBuilder(builder);
-        }
+        int[] colors = {resId2color(R.color.blue_boom_menu_btn), resId2color(R.color.red_boom_menu_btn),
+                resId2color(R.color.green_boom_menu_btn), resId2color(R.color.yellow_boom_menu_btn)};
+        initBoomMenu(texts, colors);
         mFragments = new BaseMvpFragment[4];
         mRbPic.setChecked(true);
         if (savedInstanceState == null) {
@@ -117,11 +102,49 @@ public class MainActivity extends SupportActivity {
         }
     }
 
+    private void initBoomMenu(String[] texts, int[] colors) {
+        for (int i = 0; i < mBmbContent.getButtonPlaceEnum().buttonNumber(); i++) {
+            TextOutsideCircleButton.Builder builder = new TextOutsideCircleButton.Builder()
+                    .normalImageRes(R.drawable.jelly_fish)
+                    .normalText(texts[i])
+                    .normalColor(colors[i])
+                    .textGravity(Gravity.CENTER)
+                    .rotateImage(true)
+                    .shadowEffect(true)
+                    .listener(new OnBMClickListener() {
+                        @Override
+                        public void onBoomButtonClick(int index) {
+                            switch (index) {
+                                case 0:
+                                    startActivityForResult(new Intent(MainActivity.this, LoginActivity
+                                            .class), LoginActivity.REQUEST_CODE);
+                                    break;
+                                case 1:
+                                    showFragment(FOURTH);
+                                    break;
+                                case 2:
+                                    LoginContext.getInstance().collect(MainActivity.this);
+                                    break;
+                                case 3:
+                                    LoginContext.getInstance().submit(MainActivity.this);
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                    });
+
+            mBmbContent.addBuilder(builder);
+        }
+    }
+
     private void analyseIntent() {
         Intent intent = getIntent();
-        Bundle bundle = intent.getBundleExtra(Constants.MAIN_ACTIVITY_BUNDLE);
-        mMediaBundle = bundle.getBundle(Constants.MEDIA_BUNDLE);
-        mPictureBundle = bundle.getBundle(Constants.PICTURE_BUNDLE);
+        if (intent.hasExtra(Constants.MAIN_ACTIVITY_BUNDLE)) {
+            Bundle bundle = intent.getBundleExtra(Constants.MAIN_ACTIVITY_BUNDLE);
+            mMediaBundle = bundle.getBundle(Constants.MEDIA_BUNDLE);
+            mPictureBundle = bundle.getBundle(Constants.PICTURE_BUNDLE);
+        }
     }
 
     protected int getContainerId() {
@@ -132,16 +155,33 @@ public class MainActivity extends SupportActivity {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.rb_pic:
+                setImmerseMode(false);
                 showFragment(FIRST);
                 break;
             case R.id.rb_article:
                 showFragment(SECOND);
                 break;
             case R.id.rb_music:
+                setImmerseMode(true);
                 showFragment(THIRD);
                 break;
             case R.id.rb_movie:
+                setImmerseMode(true);
                 showFragment(FOURTH);
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case LoginActivity.REQUEST_CODE:
+                if (resultCode == RESULT_OK) {
+                    // 登录成功
+                }
+                break;
+            default:
                 break;
         }
     }
@@ -150,6 +190,24 @@ public class MainActivity extends SupportActivity {
         if (position != showingPosition) {
             showHideFragment(mFragments[position], mFragments[showingPosition]);
             showingPosition = position;
+        }
+    }
+
+    private int resId2color(int resId) {
+        return ContextCompat.getColor(this, resId);
+    }
+
+    public void setImmerseMode(boolean setImmerse) {
+        if (Build.VERSION.SDK_INT >= 21) {
+            if (setImmerse) {
+                int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+                mDecorView.setSystemUiVisibility(option);
+                getWindow().setStatusBarColor(Color.TRANSPARENT);
+            } else {
+                mDecorView.setSystemUiVisibility(mDefaultUIFlag);
+                getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.main_background));
+            }
         }
     }
 }
